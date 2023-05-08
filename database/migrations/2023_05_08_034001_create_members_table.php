75cab5e2aa88afe886203190b291e35daf273154
spoon8971 <jzuoteng433@gmail.com>
2023-05-08 16:17:53 +0900

    Brezze 各種テーブル＆コントローラ実装

---
+++ database/migrations/2023_05_08_034001_create_members_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('comment', 200);
+            $table->timestamps();
+            $table->integer('point');
+            $table->softDeletes();
+           
+            //img_path
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+};
